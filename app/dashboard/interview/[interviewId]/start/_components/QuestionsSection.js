fb5d8c1df5d945fd7d0b4214a656cda8fa7e9244
Ekansh1309 <eku130902@gmail.com>
2024-07-21 01:45:37 +0530

    "second commit"

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.js
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.js
@@ -28,7 +28,7 @@ const QuestionsSection = ({mockInterviewQuestion,activeQuestionIndex,interviewDa
               mockInterviewQuestion.length>0 ? 
               (
                 mockInterviewQuestion.map((question,index)=>(
-                  <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index && 'bg-blue-700 text-white'}`}>Question #{index+1}</h2>
+                  <h2 key={index} className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index && 'bg-blue-700 text-white'}`}>Question #{index+1}</h2>
                 ))
               ) :
               (
